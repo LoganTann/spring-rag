0d4da8e24e9644722309bb8a7b3f2dc2db23c204
LoganTann <logon313@hotmail.fr>
2025-01-29 10:22:19 +0100

    #15 Ajout AssistantResource

--- src/main/java/fr/efrei/rag/web/rest/SampleResource.java
+++ src/main/java/fr/efrei/rag/web/rest/SampleResource.java
@@ -1,8 +1,11 @@
 package fr.efrei.rag.web.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.efrei.rag.domain.dto.DocumentDTO;
 
 @RestController
 public class SampleResource {
@@ -10,5 +13,10 @@ public class SampleResource {
     @GetMapping("/samples/{value}")
     public String hello(@PathVariable(value = "value", required = false) final String value){
         return "Hello " + value + "!";
+    }
+    @GetMapping("/samples/dto/{value}")
+    public  ResponseEntity<DocumentDTO> helloJSON(@PathVariable(value = "value", required = false) final String value){
+        var result = new DocumentDTO(value);
+        return ResponseEntity.ok().body(result);
     }
 }
